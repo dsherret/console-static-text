71a97d72d3adccbf517edfb25a013d358f3f278c
David Sherret <dsherret@gmail.com>
2026-07-25 10:27:26 -0400

    update

--- lib/rs_lib.d.ts
+++ lib/rs_lib.d.ts
@@ -2,19 +2,22 @@
 // deno-lint-ignore-file
 // deno-fmt-ignore-file
 
-export function static_text_render_once(
-  items: any,
-  cols?: number | null,
-  rows?: number | null,
-): string | undefined;
-export function strip_ansi_codes(text: string): string;
 export class StaticTextContainer {
   free(): void;
+  [Symbol.dispose](): void;
+  clear_text(cols?: number | null, rows?: number | null): string | undefined;
   constructor();
   render_text(
     items: any,
     cols?: number | null,
     rows?: number | null,
   ): string | undefined;
-  clear_text(cols?: number | null, rows?: number | null): string | undefined;
 }
+
+export function static_text_render_once(
+  items: any,
+  cols?: number | null,
+  rows?: number | null,
+): string | undefined;
+
+export function strip_ansi_codes(text: string): string;
